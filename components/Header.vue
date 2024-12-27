ff15c5281131fe7e5efc27eb89db441a7953d6be
ahmadzen022 <141709205+ahmadzen022@users.noreply.github.com>
2024-12-27 16:22:19 +0700

    Add files via upload

---
+++ components/Header.vue
@@ -0,0 +1,167 @@
+<template>
+  <div>
+    <!--  Navbar -->
+    <nav
+      class="navbar navbar-expand-lg navbar-light bg-transparent position-fixed w-100"
+    >
+      <div class="container">
+        <a class="navbar-brand" href="#hero">
+          <img
+            src="assets/logo.png"
+            alt=""
+            width="34"
+            class="d-inline-block align-text-top me-3 "/>
+          SMKN4 KOTA TASIKMALAYA
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav ms-auto">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="/">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="berita">Berita</a>
+            </li>
+
+
+            <!-- Dropdown Profil -->
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="/propil"id="navbarDropdownEkskul" role="button"
+                data-bs-toggle="dropdown" aria-expanded="false">Profile</a>
+              <ul class="dropdown-menu" aria-labelledby="navbarDropdownProfil" id="navbarDropdownProfil">
+                <li>
+                  <nuxt-link to="/propil/sejarah" class="dropdown-item">
+                    Sejarah
+                  </nuxt-link>
+                </li>
+                <li>
+                  <nuxt-link to="/propil/Visi-Misi" class="dropdown-item">
+                    Visi & Misi
+                  </nuxt-link>
+                </li>
+              </ul>
+            </li>
+            <!-- Dropdown kurikulum -->
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="kurikulum" id="navbarDropdownEkskul" role="button"
+                data-bs-toggle="dropdown" aria-expanded="false">
+                Kurikulum
+              </a>
+              <ul class="dropdown-menu" aria-labelledby="navbarDropdownEkskul" id="navbarDropdownEkskul">
+                <li><a class="dropdown-item" href="kurikulum/Kompetensi-Keahlian">Kompetensi Keahlian</a></li>
+                <!-- Tambahkan ekstrakurikuler lainnya sesuai kebutuhan -->
+              </ul>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="Galeri">Ekstrakurikuler</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="Galeri">Galeri</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+:root {
+  --pr-color: #048853;
+}
+
+body {
+  font-family: "Poppins", sans-serif;
+  background-color: #f3fff4;
+}
+/* Nav Fixed on Scroll */
+
+.nav-color {
+  background-color: #005a36;
+}
+
+/* Nav Scroll */
+
+.navbar {
+  z-index: 3;
+}
+
+nav .navbar-brand {
+  font-size: 20px;
+  font-weight: 700;
+  color: #fff !important;
+}
+
+.navbar .nav-link.active {
+  font-weight: 700;
+}
+
+.nav-link:hover {
+  background-color: var(--pr-color);
+}
+
+.navbar .nav-link {
+  font-size: 1rem;
+  color: #fff !important;
+}
+
+
+
+.button-secondary {
+  width: 133px;
+  height: 40px;
+  background-color: #fff;
+  color: var(--pr-color);
+  font-size: 1rem;
+  font-weight: 500;
+  border: none;
+}
+
+.button-lg-secondary {
+  width: 237px;
+  height: 70px;
+  background-color: #fff;
+  color: var(--pr-color);
+  border: none;
+  font-size: 20px;
+  font-weight: 700;
+}
+
+.dropdown-menu {
+  display: none;
+  position: absolute;
+  top: 100%;
+  left: 0;
+  background-color: #ffffff;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1);
+  z-index: 100;
+  min-width: 200px;
+}
+
+.dropdown-menu li {
+  width: 100%;
+}
+
+.dropdown-menu li a {
+  padding: 10px;
+}
+
+.dropdown:hover .dropdown-menu {
+  display: block;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,131 @@
+<template>
+<!-- Hero Section -->
+<section id="hero">
+  <!-- Tagline -->
+  <div class="container h-100">
+    <div class="row h-100">
+      <div class="col-lg-6 col-sm-12 hero-tagline my-auto">
+        <h1>Selamat Datang Di Website Resmi SMK Negeri 4 Tasikmalaya</h1>
+        <p><span class="fw-bold"></span>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non incidunt deserunt voluptatem fuga culpa porro consequuntur excepturi dignissimos ex ut accusamus adipisci reiciendis eligendi, provident suscipit autem recusandae mollitia aspernatur!</p>
+        <button class="button-lg-secondary me-4">Lihat selengkapnya</button>
+        <a href="#rekomendasi">
+                        <img src="assets/Right Arrow.png" alt="">
+                    </a>
+      </div>
+    </div>
+    <img src="assets/kepala sekolah.png" class="image-hero position-absolute end-0 bottom-0" alt="" />
+  </div>
+</section>
+<!-- End navbar -->
+
+</template>
+
+<style>
+:root {
+--pr-color: #048853;
+}
+
+body {
+font-family: "Poppins", sans-serif;
+background-color: #f3fff4;
+}
+/* Nav Fixed on Scroll */
+
+.nav-color {
+background-color: #005a36;
+}
+
+/* Nav Scroll */
+
+.navbar {
+z-index: 3;
+}
+
+nav .navbar-brand {
+font-size: 20px;
+font-weight: 700;
+color: #fff !important;
+}
+
+.navbar .nav-link.active {
+font-weight: 700;
+}
+
+.nav-link:hover {
+background-color: var(--pr-color);
+}
+
+.navbar .nav-link {
+font-size: 1rem;
+color: #fff !important;
+}
+
+.button-primary {
+width: 133px;
+height: 40px;
+background-color: transparent;
+border: 1px solid rgba(255, 255, 255, 0.3);
+font-size: 1rem;
+font-weight: 500;
+color: #fff;
+}
+
+.button-secondary {
+width: 133px;
+height: 40px;
+background-color: #fff;
+color: var(--pr-color);
+font-size: 1rem;
+font-weight: 500;
+border: none;
+}
+
+.button-lg-secondary {
+width: 237px;
+height: 70px;
+background-color: #fff;
+color: var(--pr-color);
+border: none;
+font-size: 20px;
+font-weight: 700;
+}
+
+/* Hero Section */
+
+#hero {
+background: linear-gradient(160deg, var(--pr-color), #000);
+height: 100vh;
+width: 100%;
+}
+
+#hero img {
+z-index: 0;
+}
+
+.image-hero {
+height: 100%;
+}
+
+.hero-tagline h1 {
+font-size: 50px;
+color: #fff;
+line-height: 72px;
+}
+
+.hero-tagline p {
+font-size: 1rem;
+color: #fff;
+margin-bottom: 60px;
+margin-top: 20px;
+line-height: 30px;
+width: 90%;
+}
+
+* {
+z-index: 2;
+}
+
+.accsent-img {
+z-index: 1;
+}
+</style>
